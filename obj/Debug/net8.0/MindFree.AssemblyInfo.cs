fbb34751280ed59a09fad1c83b8c6ce605bd46cd
brunovidal <brunovidal@DESKTOP-0S3DVLJ>
2025-06-28 10:13:48 -0300

    02.00.000

--- obj/Debug/net8.0/MindFree.AssemblyInfo.cs
+++ obj/Debug/net8.0/MindFree.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MindFree")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22f64253198770dd2e180e52d1f1b3e3ef6d486b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0088ba9d59598373d0446435b07edecca1f2cc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MindFree")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MindFree")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
